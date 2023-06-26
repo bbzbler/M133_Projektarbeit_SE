36cb99c1412d26a3e50cafdb8279ae7925ede863
bbzbler <simon.emmisberger@bbzbl-it.ch>
2023-06-26 21:01:51 +0200

    aufruf von Daten, Kallender, links in html

--- project.js
+++ project.js
@@ -2,8 +2,11 @@ var date = new Date();
 var wochentage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
 $(document).ready(setupContent);
 
+
 function setupContent()
 {
+    $("#date").text((date.getWeek()) + "-" + date.getYear());
+    getData();
     //TODO: set week and year in timetable
     //TODO: get async data 
 }
@@ -15,6 +18,7 @@ function getData()
     {
         //TODO: populate jobs
         //TODO: get items from localstorage if avaiable 
+        appendOptions(data);
     }
     }).fail(function()
     {
@@ -22,22 +26,23 @@ function getData()
     })
 }
 function appendOptions(data) {
-    $("#jobs").append(new Option("-bitte wählen-", 0))
+    $("#Berufsgruppen").append(new Option("-bitte wählen-", 0))
     $.each(data, function(_key, value) {
-        $("#jobs").append(new Option(value.beruf_name, value.beruf_id));
+        $("#Berufsgruppen").append(new Option(value.beruf_name, value.beruf_id));
     });
 }
 
+
 function checkLocalStorage() {
-    if (localStorage.getItem("job") != null) {
-        $("#jobs").val(localStorage.getItem("job"));
+    if (localStorage.getItem("Beruf") != null) {
+        $("#Berufsgruppen").val(localStorage.getItem("Beruf"));
     }
 }
 $("#Klassenauswahl").on("change", () => {
     $("#ZeitFenster").fadeOut(function() {
         localStorage.clear();
-        localStorage.setItem("job", $("#jobs").find(":selected").val());
-        loadClasses($("#jobs").find(":selected").val());
+        localStorage.setItem("Beruf", $("#Berufsgruppen").find(":selected").val());
+        loadClasses($("#Berufsgruppen").find(":selected").val());
     });
 });
 
@@ -73,18 +78,63 @@ function appendClassOptions(data) {
 
 function checkLocalStorageForClassID() {
     
-    if (localStorage.getItem("class") != null) {
-        var classId = localStorage.getItem("class")
+    if (localStorage.getItem("Klasse") != null) {
+        var classId = localStorage.getItem("Klasse")
         $("#Klassenauswahl").val(classId);
         loadTimeTable(classId);
     }
 }
 
 function loadTimeTable(classId) {
-    $("#timeTableBody").children().remove();
+    $("#contentBody").children().remove();
     if (classId != 0) {
         getTimeTable(classId)
     } else {
-        //TODO
+        //TODO:
     }
+}
+function getTimeTable(classId) {
+    $.getJSON("https://sandbox.gibm.ch/tafel.php?klasse_id=" + classId + "&woche=" + date.getWeek() + "-" + date.getFullYear(),
+        function(data) {
+            if (data.length > 0) {
+                appendTimeTable(data)
+            } else {
+                //TODO:
+            }
+        }
+
+    );
+}
+Date.prototype.getWeek = function() {
+    //getting the weeks since the first day of the year
+    let firstjan = new Date(this.getFullYear(), 0, 1);
+    return Math.ceil((((this - firstjan) / 86400000) + firstjan.getDay() + 1) / 7) - 1;
+}
+
+Date.prototype.addWeek = function() {
+    //eine woche später
+    this.setDate(this.getDate() + 7);
+}
+
+Date.prototype.substractWeek = function() {
+    //eine woche vorher
+    this.setDate(this.getDate() - 7);
+}
+
+const myDatePicker = MCDatepicker.create({
+    el: '#date',
+    selectedDate: new Date(),
+    disableWeekends: true
+})
+
+myDatePicker.onSelect((dt) => {
+    date = new Date(Date.parse(dt));
+    if (date == "Invalid Date") {
+        date = new Date();
+    }
+    updateTable();
+});
+function updateTable() {
+    $("#date").text(date.getWeek()) + "-" + date.getFullYear();
+    loadTimeTable($("#Klassenauswahl").find(":selected").val());
 }
